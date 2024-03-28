e40445b008e5f44b9802015a3dd9e88f0fb84918
JheysonJhair <jhair2003unamba@gmail.com>
2024-03-27 22:17:42 -0500

    release: primera version firenotify

--- src/api/apiFire.js
+++ src/api/apiFire.js
@@ -1,7 +1,7 @@
 export const fetchFireLocations = async () => {
   try {
     const response = await fetch(
-      'https://satlled.ccontrolz.com/satelite/conflagration',
+      'https://satlled.ccontrolz.com/satelite/getSatellite',
     );
     const json = await response.json();
     const filteredLocations = json.value.filter(
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text } from "react-native";
-import Button from "../../components/forms/Button";
-import MapView, { Marker } from "react-native-maps";
-import { useNavigation } from "@react-navigation/native";
-import { fetchFireLocations } from "../../api/apiFire";
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+import Button from '../../components/forms/Button';
+import MapView, {Marker, Circle} from 'react-native-maps';
+import {useNavigation} from '@react-navigation/native';
+import {fetchFireLocations, fetchData} from '../../api/apiFire';
 
 export default function Home() {
   const [fireLocations, setFireLocations] = useState([]);
+  const [fireCircleData, setFireCircleData] = useState([]);
   const navigation = useNavigation();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataAndCircle = async () => {
       const locations = await fetchFireLocations();
+      const circleData = await fetchData();
       setFireLocations(locations);
+      setFireCircleData(circleData);
+      console.log(fireCircleData);
     };
-    
-    fetchData();
 
-    const intervalId = setInterval(fetchData, 1000);
+    fetchDataAndCircle();
+
+    const intervalId = setInterval(fetchDataAndCircle, 1000);
     return () => clearInterval(intervalId);
   }, []);
 
-  
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -34,8 +37,7 @@ export default function Home() {
           longitude: -72.8814,
           latitudeDelta: 0.02,
           longitudeDelta: 0.02,
-        }}
-      >
+        }}>
         {fireLocations.length > 0 &&
           fireLocations.map((location, index) => (
             <Marker
@@ -46,49 +48,70 @@ export default function Home() {
               }}
               title={`Temperatura: ${location.temperature}`}
               description={`Fecha: ${location.date}`}
-              image={require("../../assets/fuego.png")}
+              style={styles.markerImage}>
+              <Image
+                source={require('../../assets/fuego.png')}
+                style={styles.markerImage}
+              />
+            </Marker>
+          ))}
+
+        {fireCircleData.length > 0 &&
+          fireCircleData.map((circle, index) => (
+            <Circle
+              key={index}
+              center={{
+                latitude: parseFloat(circle.latitud),
+                longitude: parseFloat(circle.longitud),
+              }}
+              radius={80}
+              fillColor="rgba(255, 0, 0, 0.5)"
+              strokeColor="rgba(255, 0, 0, 0.5)"
             />
           ))}
       </MapView>
-
       <View style={styles.buttonContainer}>
         <Button
           title="Notificaciones"
-          onPress={() => navigation.navigate("Notify")}
+          onPress={() => navigation.navigate('Notify')}
         />
       </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "002854",
+    backgroundColor: '#002854',
   },
   map: {
     flex: 1,
-    width: "100%",
+    width: '100%',
   },
   buttonContainer: {
-    position: "absolute",
-    textAlign: "center",
-    justifyContent: "center",
+    position: 'absolute',
+    textAlign: 'center',
+    justifyContent: 'center',
     bottom: 20,
     right: 8,
     zIndex: 1,
   },
-
   header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingVertical: 12,
-    backgroundColor: "#002854",
-    width: "100%",
+    backgroundColor: '#002854',
+    width: '100%',
   },
   title: {
     fontSize: 21,
-    fontWeight: "bold",
-    color: "white",
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  markerImage: {
+    width: 30,
+    height: 30,
   },
 });
